755ec71ee8035163d5b1ce09b6d00fcb3af7b149
Erivaldo-Montes <erivaldomontez@gmail.com>
2022-12-28 19:53:12 -0300

    refactor routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/defaultLayout'
-import { Home } from './pages'
+import { Home } from './pages/Home'
+import { Post } from './pages/Post'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/post" element={<h1>post</h1>} />
+        <Route path="/post/:slug/:post" element={<Post />} />
       </Route>
     </Routes>
   )
