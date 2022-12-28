2eefbc7d28eb8a2c7c3e2fd751566e3658809d2c
Erivaldo-Montes <erivaldomontez@gmail.com>
2022-12-28 19:50:34 -0300

    axios client

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-export const axiosFetch = axios.create({
-  baseURL: 'http://api.github.com',
+export const API = axios.create({
+  baseURL: 'https://api.github.com',
 })
